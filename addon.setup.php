348a71bca4b30d7af0fb9be89064b0c5d7f8ec62
Jared Chesebro <jared.chesebro@mc2design.com>
2021-08-20 16:54:03 -0700

    Fixed plugin description.

--- addon.setup.php
+++ addon.setup.php
@@ -4,7 +4,7 @@ return array(
 	'author'         => 'Matt Nicholson',
 	'author_url'     => 'https://github.com/mattnicholson/EEgrouper',
 	'name'           => 'Grouper',
-	'description'    => 'Allows you to create croups within entry feeds based on a count',
+	'description'    => 'Allows you to create groups within channel entries loops based on a count',
 	'version'        => '2.0',
 	'namespace'      => 'Nicholson\Grouper',
 	'settings_exist' => FALSE
